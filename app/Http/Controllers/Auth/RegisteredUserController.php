a2f242eb0eab35438a78589ccef779e91dc632a9
Karim <ytghost9@gmail.com>
2023-05-18 21:40:09 +0300

    fixed users and isae attribute

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,14 +32,22 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $email = $request->input('email');
+        $isISAE = false;
+
+        if (strpos($email, '@isae') !== false) {
+            $isISAE = true;
+        }
+    
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'isISAE' => $isISAE,
         ]);
 
         event(new Registered($user));
