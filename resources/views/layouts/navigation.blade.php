5dfd13f6b68d0b0970ffaf69d077296528351762
Ouaydah <119103172+S-Ouaydah@users.noreply.github.com>
2023-05-18 22:27:08 +0300

    explore route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('explore')" :active="request()->routeIs('explore')">
+                        {{ __('Explore') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/explore', function () {
+    return view('explore');
+})->name('explore');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
