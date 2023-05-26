6713276e6a0c9d9c3cac1ce8236a939fcf68226e
Ouaydah <119103172+S-Ouaydah@users.noreply.github.com>
2023-05-26 20:24:10 +0300

    Squashed commit of the following:
    commit b720a7c261799f31c7d83b6160bbcc77f49bff56
    Author: Ihab Mheich <69162168+ihabmh@users.noreply.github.com>
    Date:   Fri May 26 19:58:24 2023 +0300
    
        explore button works now !!!
    
    commit 85f3ebae98c7253f1fece1c4778736c055186ea3
    Author: Ihab Mheich <69162168+ihabmh@users.noreply.github.com>
    Date:   Thu May 25 13:56:51 2023 +0300
    
        wsp
    
    commit 3cd143b0ebd7a5b59bca966e07aa4281c45fd362
    Author: Ihab Mheich <69162168+ihabmh@users.noreply.github.com>
    Date:   Thu May 25 13:41:16 2023 +0300
    
        erwr
    
    commit 9532942ca473569f00b04db1b1d12ffbfe330211
    Author: Ihab Mheich <69162168+ihabmh@users.noreply.github.com>
    Date:   Wed May 24 22:33:17 2023 +0300
    
        bruh
    
    commit 1fa57f1335dda94cc174d1059f454344d05afbb8
    Author: Ihab Mheich <69162168+ihabmh@users.noreply.github.com>
    Date:   Tue May 23 20:21:17 2023 +0300
    
        first

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,23 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        'https://cdn.tailwindcss.com',
+      
+        
     ],
 
     theme: {
+
+        
+
+    
+
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
+        
     },
 
     plugins: [require('@tailwindcss/forms')],
