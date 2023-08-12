dbe18c6f28cd67be86a3924b86e610305260b31c
Ihab Mheich <69162168+ihabmh@users.noreply.github.com>
2023-08-12 20:06:47 +0300

    auth pages design + responsive + all images are now local

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,8 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+ <h1 class="font-black text-3xl text-center">Forgot your password?</h1>
+
+    <div class="mb-4 text-sm text-gray-600 mt-4">
+        {{ __('No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
     <!-- Session Status -->
@@ -17,9 +19,9 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <button class="bg-black text-white px-5 py-3 font-bold rounded-3xl w-full">
                 {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+            </button>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,35 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/scss/app.scss','resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <!-- Scripts -->
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center  sm:pt-6 bg-gray-100">
+        {{-- <div>
+            <a href="/">
+                <img src="{{ asset('logo-black.png') }}">
+            </a>
+        </div> --}}
+
+        <div class="flex flex-col w-full sm:flex-row sm:w-[90%] lg:w-[70%] xl:w-[55%] 3xl:w-[45%] sm:h-[700px] sm:mt-6 shadow-md rounded-none sm:rounded-3xl">
+            <img class="w-full sm:w-[50%]  sm:rounded-s-3xl object-cover"
+             src="{{ asset('auth-pic5.jpg') }}">
+            <div class="w-full sm:w-[50%]  px-[8%] py-16 bg-white  overflow-hidden rounded-none sm:rounded-e-3xl flex flex-col justify-center ">
+            
                 {{ $slot }}
             </div>
         </div>
-        <div class="container w-[30%]">
-            <audio crossorigin playsinline>
-                <source src="{{ asset('storage/Broken Elegance - I Need You.mp3') }}" type="audio/mp3">
-            </audio>
-        </div>
-    </body>
+
+
+    </div>
+    {{-- <div class="container w-[30%] ">
+        <audio crossorigin playsinline>
+            <source src="{{ asset('storage/Broken Elegance - I Need You.mp3') }}" type="audio/mp3">
+        </audio>
+    </div> --}}
+</body>
+
 </html>
